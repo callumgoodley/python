35c40077e11fde9e996a267a817706c135bb06d5
CallumGoodley <callumgoodley@btinternet.com>
2020-06-03 19:55:10 +0100

    cotd_3.py added with passing test file

---
+++ Code/cotd_3.py
@@ -0,0 +1,9 @@
+# Question: With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary. Suppose the following input is supplied to the program: 8 Then, the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+
+def generate_dictionary(num):
+    new_dictionary = {}
+
+    for i in range(1, num + 1):
+        new_dictionary[i] = i*i
+    return new_dictionary
---
+++ Tests/test_cotd_3.py
@@ -0,0 +1,7 @@
+import pytest
+from Code import cotd_3
+
+
+def test_cotd_3():
+    assert cotd_3.generate_dictionary(
+        8) == {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
